8f3360f8decc4382e87cd6205129244c36188695
solomon54 <solomontsehay54@gmail.com>
2026-01-17 01:17:06 +0300

    update dependancyes and refactore the fyle names

--- prodev-mobile-app-0x05/app/(home)/index.tsx
+++ prodev-mobile-app-0x05/app/(home)/index.tsx
@@ -1,4 +1,4 @@
-import PropertyListing from "@/components/PropretyListing";
+import PropertyListing from "@/components/PropertyListing";
 import { styles } from "@/styles/_mainstyle";
 import { Feather } from "@expo/vector-icons";
 import {
@@ -10,7 +10,7 @@ import {
   Dimensions,
   TouchableHighlight,
 } from "react-native";
-import { FILTERS, SAMPLE_DATA } from "@/constants";
+import { FILTERS, SAMPLE_DATA } from "@/constants/data";
 const Home = () => {
   return (
     <View style={styles.container}>
